f632dca7cf7866f879391d31f74fa8c266e73b25
Josh Salway <josh.salway@gmail.com>
2026-09-11 19:22:20 +1000

    Remove unused Sanctum demo route

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,7 @@
 
 use App\Http\Controllers\Api\BookingController;
 use App\Http\Controllers\Api\VehicleController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::get('/vehicles/availability', [VehicleController::class, 'availability']);
 Route::post('/bookings', [BookingController::class, 'store'])->middleware('auth:sanctum');
