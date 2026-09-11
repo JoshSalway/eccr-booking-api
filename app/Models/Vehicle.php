e5a9df752059f511316ab8bb8da5e02fada14612
Josh Salway <josh.salway@gmail.com>
2026-09-11 19:06:02 +1000

    Fix import order in Vehicle model

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
+use Database\Factories\VehicleFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Vehicle extends Model
 {
-    /** @use HasFactory<\Database\Factories\VehicleFactory> */
+    /** @use HasFactory<VehicleFactory> */
     use HasFactory;
 
     protected $fillable = [
